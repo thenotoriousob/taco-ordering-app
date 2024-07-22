23b3d1fc7e290b1ec7e3a27a7024310055a25249
thenotoriousob <32715357+thenotoriousob@users.noreply.github.com>
2024-07-22 18:25:41 +0100

    Initial commit of the html and css

---
+++ data.js
@@ -0,0 +1,23 @@
+const menuArray = [
+    {
+        name: "Tacos",
+        ingredients: ["pepperoni", "mushrom", "mozarella"],
+        id: 0,
+        price: 14,
+        emoji: "🌮"
+    },
+    {
+        name: "Burrito",
+        ingredients: ["beef", "cheese", "lettuce"],
+        price: 12,
+        emoji: "🌯",
+        id: 1
+    },
+        {
+        name: "Beer",
+        ingredients: ["grain, hops, yeast, water"],
+        price: 12,
+        emoji: "🍺",
+        id: 2
+    }
+]
